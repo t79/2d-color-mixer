6a8d27afe9e5814aa87f6e88c53eba59ca218dd2
Terje Urnes <terje@urnes.net>
2022-02-09 22:45:56 +0100

    Adding javascript file

---
+++ 2d-mixer.js
@@ -0,0 +1,13 @@
+document.addEventListener('readystatechange', function () {
+    if (document.readyState === 'interactive') {
+        initColorMixer();
+    }
+});
+
+t79CM2 = {
+
+}
+
+function initColorMixer() {
+
+}
